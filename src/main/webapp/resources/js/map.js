b4cd59c73dd604e20b40c3e2575478e687fc69aa
dbckd999 <dbckd999@naver.com>
2022-04-29 17:48:05 +0900

    현재위치 표시기능 추가

--- src/main/webapp/resources/js/map.js
+++ src/main/webapp/resources/js/map.js
@@ -27,17 +27,11 @@ function onMapClick(e) {
 	    .openOn(map);
 }
 
+navigator.geolocation.getCurrentPosition(
+	function(location) {
+		var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+		var marker = L.marker(latlng).addTo(map);
+});
+
 map.on('click', onMapClick);
-
-
-$(".btn").click(function() 
-{ $("#menu").addClass("open"); }); 
-$(".close").click(function() { $("#menu").
-removeClass("open"); });
-
-$(".btn").click(function () { $("#menu,.page_cover,html").addClass("open"); 
- window.location.hash = "#open"; }); 
-window.onhashchange = function () 
-{ if (location.hash != "#open") { 
-	 $("#menu,.page_cover,html").removeClass("open");  } };
 
