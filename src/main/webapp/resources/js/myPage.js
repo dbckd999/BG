74b7eb4fbfbcdd61f95c341a1c0f1a506a1c3cd1
JHS <uolk0600@naver.com>
2022-04-29 15:37:14 +0900

    ^_)^

---
+++ src/main/webapp/resources/js/myPage.js
@@ -0,0 +1,11 @@
+$(".btn").click(function() 
+{ $("#menu").addClass("open"); }); 
+$(".close").click(function() { $("#menu").
+removeClass("open"); });
+
+$(".btn").click(function () { $("#menu,.page_cover,html").addClass("open"); 
+ window.location.hash = "#open"; }); 
+window.onhashchange = function () 
+{ if (location.hash != "#open") { 
+	 $("#menu,.page_cover,html").removeClass("open");  } };
+
