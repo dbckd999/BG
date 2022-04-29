e06e3edbeac96b57762f2643399cc33853b75f30
hoya09 <ds548417@naver.com>
2022-04-29 16:06:06 +0900

    카운트다운

--- src/main/java/study/projectBG/BG/controller/MapController.java
+++ src/main/java/study/projectBG/BG/controller/MapController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MapController {
-	@GetMapping("/Map")
-	public String mainMap() {
+	@GetMapping("/map")
+	public String mainmap() {
 		return "/map/map";
 	}
 }
