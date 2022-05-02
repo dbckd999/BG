6af07ef07ca2046d12869134e88ede15a3fb4788
dbckd999 <dbckd999@naver.com>
2022-05-02 16:08:34 +0900

    지도이벤트 수정중

--- src/main/webapp/resources/js/map.js
+++ src/main/webapp/resources/js/map.js
@@ -1,6 +1,7 @@
 //cdn으로 불러온 L객체에서 지도 로드.
-var map = L.map('map').setView([35.861509, 128.572311], 13);
-
+var map = L.map('map').setView([35.892805, 128.525276], 13);
+//주변 화장실을 배열로 담게 됩니다.
+var restRooms;
 //지도타일 생성
 var Jawg_Terrain = L.tileLayer('https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
 	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -17,14 +18,25 @@ function onMapClick(e) {
 	    .setContent('You clicked the map at ' + e.latlng.toString())
 	    .openOn(map);
 }
-
 //지도 클릭이벤트 등록
-map.on('click', onMapClick);
+//map.on('click', onMapClick);
 
-//현재위치 지도에 표시
-navigator.geolocation.getCurrentPosition(
-	function(location) {
-		var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
-		var marker = L.marker(latlng).addTo(map);
-});
+//현재위치 저장
+var myLocationMarker;
 
+//현재위치 표시버튼 누르면 실시간 업데이트
+function updateMyLocation(){
+	navigator.geolocation.getCurrentPosition(
+		function(location) {
+			var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+			myLocationMarker = L.marker(latlng).addTo(map);
+			map.setView(latlng);
+		});
+}
+updateMyLocation();
+
+// create control and add to map
+var lc = L.control.locate().addTo(map);
+
+// request location update and set location
+lc.start();
