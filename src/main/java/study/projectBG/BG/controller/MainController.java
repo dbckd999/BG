54e7607af3c504ea870570649a867f63691e05a3
dbckd999 <dbckd999@naver.com>
2022-05-04 09:03:39 +0900

    test_test

--- src/main/java/study/projectBG/BG/controller/MainController.java
+++ src/main/java/study/projectBG/BG/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
+	//testest
 	@GetMapping("/")
 	public String main() {
 		System.out.println("main call");
