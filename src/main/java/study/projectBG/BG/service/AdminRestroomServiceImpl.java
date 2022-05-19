9be3dbd0fc616c436ebb23ce7ed31cd8d471c1a1
dbckd999 <dbckd999@naver.com>
2022-05-19 10:01:42 +0900

    관리자 화장실관련기능 추상 메서드 제작

---
+++ src/main/java/study/projectBG/BG/service/AdminRestroomServiceImpl.java
@@ -0,0 +1,41 @@
+package study.projectBG.BG.service;
+
+import java.util.List;
+
+import study.projectBG.BG.model.MapDTO;
+
+//관리자가 화장실 정보를 운영할수있도록 만드는 인터페이스입니다.
+public class AdminRestroomServiceImpl implements AdminRestroomService {
+
+	
+	
+	@Override
+	public List<MapDTO> showRestroomList(int index, int cMaxList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MapDTO showARestroom(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertRestroom() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateRestroom(MapDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteRestroom(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
